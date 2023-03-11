2f44a353fb03034f11cef53ebe82d32e503b788d
B-Jhane Francisco Palomar <117366896+jhanebb97@users.noreply.github.com>
2023-03-12 03:44:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,4 +11,14 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
-
+const IT_3102 = [
+{
+code: "20-08732",
+name: "Palomar, B-Jhane F.",
+activities: {
+website: {
+link: "https://github.com/jhanebb97/B-Jhane-Palomar/",
+code: "https://github.com/jhanebb97/B-Jhane-Palomar",
+},
+},
+},
